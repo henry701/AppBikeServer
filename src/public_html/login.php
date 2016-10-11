e460541a28f51da36b17a9143b7869f176b2c3a3
henry701 <henry701@users.noreply.github.com>
2016-10-11 07:54:50 -0300

    Add JSON de debug e erro direcionado pro log

--- src/public_html/login.php
+++ src/public_html/login.php
@@ -11,42 +11,49 @@ if(logval(FALSE) === TRUE)
 else
 {
 	$DBInstance = PDO_MODDED::getInstance();
+	
+	// Contra bruteforce
+	sleep(1);
 
 	$stmt = $DBInstance->prepare("SELECT id, senha FROM appb_usuarios WHERE email = :email LIMIT 1");
 	$stmt->bindValue(':email', $_POST['user'], PDO::PARAM_STR);
 	$result = $stmt->execute();
 
 	if($result === FALSE)
-		$DBInstance::Debug_PDO_Error($stmt, TRUE);
-
-	$j = $stmt->fetch(PDO::FETCH_ASSOC);
-
-	// Contra bruteforce
-	sleep(1);
-
-	if($j === FALSE)
 	{
+		$DBInstance::Debug_PDO_Error($stmt);
 		$ReturnArr['result'] = FALSE;
-		$ReturnArr['message'] = "Usuário não existente!";
+		$ReturnArr['message'] = "Erro interno do servidor";
+		$ReturnArr['data'] = "File: __FILE__\nLine: __LINE__";
 	}
 	else
 	{
-		$PasswordCheck = CheckCripto($_POST['senha'], 10, '', $j['senha']);
+		$j = $stmt->fetch(PDO::FETCH_ASSOC);
 
-		if($PasswordCheck === TRUE)
+		if($j === FALSE)
 		{
-			$_SESSION["loggedin"] = TRUE;
-			$_SESSION["userid"] = $j['id'];
-			$_SESSION["passconf"] = FALSE;
-			$_SESSION["last_activity"] = time();
-			session_write_close();
-			$ReturnArr['result'] = TRUE;
-			$ReturnArr['message'] = "Logado com sucesso!";
+			$ReturnArr['result'] = FALSE;
+			$ReturnArr['message'] = "Usuário não existente!";
 		}
 		else
 		{
-			$ReturnArr['result'] = FALSE;
-			$ReturnArr['message'] = "Senha incorreta!";
+			$PasswordCheck = CheckCripto($_POST['senha'], 10, '', $j['senha']);
+
+			if($PasswordCheck === TRUE)
+			{
+				$_SESSION["loggedin"] = TRUE;
+				$_SESSION["userid"] = $j['id'];
+				$_SESSION["passconf"] = FALSE;
+				$_SESSION["last_activity"] = time();
+				session_write_close();
+				$ReturnArr['result'] = TRUE;
+				$ReturnArr['message'] = "Logado com sucesso!";
+			}
+			else
+			{
+				$ReturnArr['result'] = FALSE;
+				$ReturnArr['message'] = "Senha incorreta!";
+			}
 		}
 	}
 }
