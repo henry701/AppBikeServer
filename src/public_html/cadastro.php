f6e839a5c8f43ce29d8f57b07f4bc61983e0d0cc
henry701 <henry701@users.noreply.github.com>
2016-10-11 08:05:16 -0300

    Arrumar a caca que esse mlk fez dormindo

--- src/public_html/cadastro.php
+++ src/public_html/cadastro.php
@@ -1,14 +1,52 @@
 <?php
 
-ini_set("display_errors",1);
+require_once '../includes/core.php';
 
-require '../../src/includes/functions.php';
+$ReturnArr = Array();
 
-$newClientData = array(
-                  "email"=>"'{$_POST['email']}'",
-                  "senha"=>"'{$_POST['senha']}'",
-                  "nome"=>"'{$_POST['nome']}'"
-                );
+/*
+$_POST['email']
+$_POST['senha']
+$_POST['nome']
+*/
 
-insertNewUsers($newClientData);
+if(logval(FALSE) === TRUE)
+{
+	$ReturnArr['result'] = TRUE;
+	$ReturnArr['message'] = "Já está logado!";
+}
+else
+{
+	$DBInstance = PDO_MODDED::getInstance();
+	$stmt = $DBInstance->prepare("SELECT nome, email FROM appb_usuarios WHERE email = :email OR nome = :nome LIMIT 1");
+	$stmt->bindValue(':email', $_POST['email'], PDO::PARAM_STR);
+	$stmt->bindValue(':nome', $_POST['nome'], PDO::PARAM_STR);
+	$result = $stmt->execute();
+	if($result === FALSE)
+	{
+		$DBInstance::Debug_PDO_Error($stmt, TRUE);
+	}
+	else
+	{
+		$j = $stmt->fetch(PDO::FETCH_ASSOC);
 
+		if($j !== FALSE)
+		{
+			$ReturnArr['result'] = FALSE;
+			$ReturnArr['message'] = "Este e-mail ou nome já existe em nosso sistema!";
+			
+		}
+		else
+		{
+			$Password = CryptBlowFish($_POST['senha'], 10);
+			$Password = $Password['cripto'];
+			$stmt = $DBInstance->prepare("INSERT INTO appb_usuarios (nome, email, senha) VALUES (:nome, :email, :senha)");
+			$stmt->bindValue(':senha', $Password, PDO::PARAM_STR);
+			$stmt->bindValue(':email', $_POST['email'], PDO::PARAM_STR);
+			$stmt->bindValue(':nome', $_POST['nome'], PDO::PARAM_STR);
+			$result = $stmt->execute();
+		}
+	}
+}
+
+JsonResponse($ReturnArr);
